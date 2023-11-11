91aa3dac4873a562bf74140aad083583f7ca5153
LilGamerPlayz <thelittlebotengineer@gmail.com>
2023-11-10 19:43:53 -0800

    Update 0.0.02

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import Head from 'next/head';
 import React from 'react';
+import styles from '../styles/home.module.scss';
+import Footer from '@/templates/footer';
+import { Work_Sans } from 'next/font/google';
+
+const Work_Sans_300 = {
+    weight: "300",
+    style: 'normal',
+    subsets: ['latin']
+}
 
 const HomePage: React.FC = (): JSX.Element => {
     try {
         return (
-            <div>
+            <div className={styles.homePageMainDiv}>
                 <Head>
                     <title>enVId Tech - Home Page</title>
+                    <link href="../public/favicon/favicon.ico" type="image/x-icon" rel="icon"/>
                 </Head>
-                <h1>Home Page</h1>
-                <h6>Latest Update: 11-9-2023, Revision 0.0.01</h6>
+                <div className={styles.titleDiv} id="homePageMainDivHeader">
+                    <h1>Home Page</h1>
+                    <h6>Latest Update: 11-9-2023, Revision 0.0.01</h6>
+                </div>
+                <Footer />
             </div>
         )
     } catch (err: unknown) {
---
+++ templates/footer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './scss/footer.module.scss';
+import { Work_Sans } from 'next/font/google';
+
+const Work_Sans_300 = {
+    weight: "300",
+    style: 'normal',
+    subsets: ['latin']
+}
+
+const Footer: React.FC = (): JSX.Element => {
+    try {
+        return (
+            <div className={`${styles.footerDiv}`}>
+                <div className={`${styles.footerLeft}`}>
+                    <p className={`${styles.label}`}></p>
+                </div>
+                <div className={`${styles.footerRight}`}>
+                    <p className={`${styles.brandName}`}>enVId Tech</p>
+                    <p>Latest Update: 11-10-2023, Revision 0.0.01</p>
+                </div>
+            </div>
+        )
+    } catch (err: unknown) {
+        console.error(err as string);
+
+        return (
+            <div>
+                <div>
+                    <h1></h1>
+                    <h6>Latest Update: 11-9-2023, Revision 0.0.02</h6>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer;
