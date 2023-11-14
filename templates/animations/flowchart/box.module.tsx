e6fb567d42301cf1561db4627e39468747ab1a09
Erick <thelittlebotengineer@gmail.com>
2023-11-14 12:29:10 -0800

    Update 0.0.41

--- templates/animations/flowchart/box.module.tsx
+++ templates/animations/flowchart/box.module.tsx
@@ -18,7 +18,13 @@ interface BoxProps {
 const Box: React.FC<BoxProps> = (props: BoxProps) => {
   const box = React.useRef<HTMLHeadingElement | null>(null);
 
-  const [myPathScrolled, setMyPathScrolled] = React.useState<boolean>(false);
+  const [myPathScrolled, setMyPathScrolled] = React.useState<boolean | null>(false);
+  const [xPos, setXPos] = React.useState<number | null>(0);
+  const [yPos, setYPos] = React.useState<number | null>(0);
+  const [dX, setDX] = React.useState<number | null>(0);
+  const [dY, setDY] = React.useState<number | null>(0);
+  const [mouseRaf, setMouseRaf] = React.useState<number | null>(null);
+  const [gradMoveRaf, setGradMoveRaf] = React.useState<number | null>(null);
 
   const onScroll = React.useCallback((): void => {
     setMyPathScrolled(props.myPathScrolled);
@@ -57,9 +63,9 @@ const Box: React.FC<BoxProps> = (props: BoxProps) => {
     if (props.myPathScrolled) {
       setTimeout((): void => {
         animateText(box, props.text, 100);
-      }, 550 + 175 * props.delay);
+      }, 550 + 175 * props.delay as number);
     }
-  }, [props.text, props.myPathScrolled]);
+  }, [props.text, props.myPathScrolled, props.delay]);
 
 
   React.useEffect(() => {
@@ -67,8 +73,56 @@ const Box: React.FC<BoxProps> = (props: BoxProps) => {
     return () => window.removeEventListener("scroll", onScroll);
   }, [onScroll]);
 
+  React.useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (!mouseRaf) {
+        setMouseRaf(requestAnimationFrame(() => {
+          const windowWidth: number = window.innerWidth;
+          const windowHeight: number = window.innerHeight;
+
+          const mouseXpercentage: number = Math.round(event.pageX / windowWidth * 100);
+          const mouseYpercentage: number = Math.round(event.pageY / windowHeight * 100);
+
+          if (xPos && yPos) {
+            setDX(mouseXpercentage - xPos);
+            setDY(mouseYpercentage - yPos);
+
+            setMouseRaf(null);
+          }
+        }));
+      }
+
+      if (!gradMoveRaf) {
+        setGradMoveRaf(requestAnimationFrame(gradMove));
+      }
+    };
+
+    const gradMove = () => {
+      if (xPos && yPos && dX && dY) {
+        setXPos(xPos + (dX / 50));
+        setYPos(yPos + (dY / 50));
+
+        const absX: number = Math.abs(xPos - dX);
+        const absY: number = Math.abs(yPos - dY);
+        if (absX < 1 && absY < 1) {
+          setGradMoveRaf(null);
+          console.log("stop");
+        } else {
+          setGradMoveRaf(requestAnimationFrame(gradMove));
+          console.log("repeat");
+        }
+      }
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [xPos, yPos, dX, dY, mouseRaf, gradMoveRaf]);
+
   return (
-    <div className={`${props.boxType === 2 ? styles.box2 : styles.box} ${myPathScrolled ? styles[`animation-delay-${props.delay}`] : ""}`}>
+    <div className={`${props.boxType === 2 ? styles.box2 : styles.box} ${myPathScrolled ? styles[`animation-delay-${props.delay}`] : ""}`} style={{ background: `radial-gradient(at ${xPos}% ${yPos}%, #e6e6e6, #1e1e1e)` }}>
       <h1 className={`${styles.boxH1} ${Work_Sans_300.className}`} ref={box}></h1>
     </div>
   )
