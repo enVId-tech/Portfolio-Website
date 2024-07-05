befa5872bc071abc9444307cac0c6eb8d373ca4f
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-04 21:09:32 -0700

    2-feat. Added main page loader

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+const HomePage: React.FC = (): React.JSX.Element => {
+    return (
+        <>
+            <Head>
+            <title>enVId Tech - Home Page</title>
+            </Head>
+            <h1>Home</h1>
+        </>
+    );
+};
