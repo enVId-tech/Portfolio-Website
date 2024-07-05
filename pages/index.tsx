31c99d8eb3f881ff0e68cfbcc85e4cbd84957600
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-05 15:27:46 -0700

    9-feat. Worked on footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Head from "next/head";
 import styles from '@/styles/home.module.scss';
 import TitleDiv from "@/templates/pages/titleDiv.section.tsx";
-import About from "@/templates/pages/about.section";
+import About from "@/templates/pages/about.section.tsx";
+import Footer from "@/templates/pages/footer.section.tsx";
 
 const HomePage: React.FC = (): React.JSX.Element => {
     type TitlePropsType = { titlePlate: string; subTitlePlate: string; titlePlateDelay: number; subTitlePlateDelay: number; timeBetweentitleAndSubTitle?: number; waitTime?: number; };
     type AboutPropsType = { aboutScrollHeight: number; aboutText: string[]; };
+    type FooterPropsType = { latestUpdate: string; dateUpdated: string; brandName: string; };
 
     // Title Div
     const TitleProps: TitlePropsType = {
@@ -22,11 +24,18 @@ const HomePage: React.FC = (): React.JSX.Element => {
     const AboutProps: AboutPropsType = {
         aboutScrollHeight: 700,
         aboutText: [
-            "3 years of full stack web development experience.",
+            "4 years of full stack web development experience.",
             "Specialized in React, MongoDB, and Node.js.",
-            "Self-studied in software.",
+            "Self-studied software engineering, 6 years.",
             "Making innovative projects for the community."
         ]
+    }
+
+    // Footer
+    const FooterProps: FooterPropsType = {
+        latestUpdate: 'Update 9',
+        dateUpdated: "July 5th, 2024",
+        brandName: "enVId Tech"
     }
 
     return (
@@ -43,7 +52,11 @@ const HomePage: React.FC = (): React.JSX.Element => {
                 waitTime={TitleProps.waitTime as number}
             />
             <About aboutScrollHeight={AboutProps.aboutScrollHeight as number} aboutText={AboutProps.aboutText as string[]} />
-
+            <Footer
+                latestUpdate={FooterProps.latestUpdate as string}
+                dateUpdated={FooterProps.dateUpdated as string}
+                brandName={FooterProps.brandName as string}
+            />
         </div>
     );
 };
---
+++ templates/pages/footer.section.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from '@/styles/footer.module.scss';
+import { Work_Sans } from 'next/font/google';
+
+const Work_Sans_300 = Work_Sans({
+    weight: "300",
+    style: 'normal',
+    subsets: ['latin']
+});
+
+interface FooterProps {
+    latestUpdate: string;
+    dateUpdated: string;
+    brandName: string;
+}
+
+const Footer: React.FC<FooterProps> = (props: FooterProps): JSX.Element => {
+    const latestUpdate: string = `Latest Update: ${props.dateUpdated}, Revision ${props.latestUpdate}`;
+    const brandName: string = props.brandName;
+    try {
+        return (
+            <div className={`${styles.footerDiv}`}>
+                <div className={`${styles.footerLeft}`}>
+                    <p className={`${styles.name} ${Work_Sans_300.className}`}>Erick Tran</p>
+                </div>
+                <div className={`${styles.footerRight} ${Work_Sans_300.className}`}>
+                    <p className={`${styles.brandName} ${Work_Sans_300.className}`}>{brandName}</p>
+                    <p className={`${styles.latestUpdateLabel} ${Work_Sans_300.className}`}>{latestUpdate}</p>
+                </div>
+            </div>
+        )
+    } catch (err: unknown) {
+        console.error(err as string);
+        return (
+            <div>
+                <div>
+                    <h1>{brandName}</h1>
+                    <h6>{latestUpdate}</h6>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer;
