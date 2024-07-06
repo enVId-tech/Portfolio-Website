7d071ad25cb5a12bbe68bccd20865d2688cd97a6
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-06 00:29:05 -0700

    17-feat. Worked on templating projects files

---
+++ templates/pages/projects.section.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "@/styles/projects.module.scss";
+import { Work_Sans, Montserrat } from "next/font/google";
+import ProjectCard from "@/components/ProjectCard.tsx";
+
+const Work_Sans_300 = Work_Sans({
+    weight: "300",
+    style: "normal",
+    subsets: ["latin"],
+});
+
+const Montserrat_400 = Montserrat({
+    weight: "400",
+    style: "normal",
+    subsets: ["latin"],
+});
+
+interface ProjectsProps {
+    projects: {
+        title: string;
+        description: string;
+        techStack: string[];
+        githubLink: string;
+        liveLink: string;
+    }[];
+}
+
+const Projects: React.FC<ProjectsProps> = (props: ProjectsProps): JSX.Element => {
+    return (
+        <div className={styles.projectsDiv} id="projects">
+            <p className={`${styles.sectionHeading} ${Work_Sans_300.className}`}>Projects</p>
+            <div className={styles.projectCards}>
+                {props.projects.map((project, index) => {
+                    return (
+                        <ProjectCard
+                            key={index}
+                            title={project.title}
+                            description={project.description}
+                            techStack={project.techStack}
+                            githubLink={project.githubLink}
+                            liveLink={project.liveLink}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
---
+++ templates/pages/resources.section.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "@/styles/resources.module.scss";
+import { Work_Sans } from "next/font/google";
+import ResourceSlideAnim from "@/templates/other/resourceSlideAnim.ts";
+
+const Work_Sans_300 = Work_Sans({
+    weight: "300",
+    style: "normal",
+    subsets: ["latin"],
+});
+
+interface ResourcesProps {
+    resources: {
+        title: string;
+        description: string;
+        link: string;
+    }[];
+}
+
+const Resources: React.FC<ResourcesProps> = (props: ResourcesProps): JSX.Element => {
+    return (
+        <div className={styles.resourcesDiv} id="resources">
+            <p className={`${styles.sectionHeading} ${Work_Sans_300.className}`}>Resources</p>
+            <div className={styles.resourceCards}>
+                {props.resources.map((resource, index) => {
+                    return (
+                        <ResourceSlideAnim
+                            key={index}
+                            title={resource.title}
+                            description={resource.description}
+                            link={resource.link}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
