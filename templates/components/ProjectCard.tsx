204f8acc5888b8838232aef63c574ae932f5c63a
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-06 18:47:00 -0700

    24-style. Continued styling resources

--- templates/components/ProjectCard.tsx
+++ templates/components/ProjectCard.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import styles from '@/styles/projectCard.module.scss';
+import { Work_Sans } from "next/font/google";
+
+const Work_Sans_300 = Work_Sans({
+    weight: "300",
+    style: 'normal',
+    subsets: ['latin']
+});
 
 interface ProjectCardProps {
     title: string;
@@ -12,9 +19,9 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = (props: ProjectCardProps): JSX.Element => {
     return (
-        <div className={styles.projectCard}>
-            <h3 className={styles.projectTitle}>{props.title}</h3>
-            <p className={styles.projectDescription}>{props.description}</p>
+        <div className={`${styles.projectCard} ${Work_Sans_300.style}`} id="projects">
+            <h3 className={`${styles.projectTitle} ${Work_Sans_300.style}`}>{props.title}</h3>
+            <p className={`${styles.projectDescription} ${Work_Sans_300.style}`}>{props.description}</p>
             <iframe src={props.embed} title={props.title} frameBorder="0" allowFullScreen className={styles.embed}></iframe>
             <a href={props.link} target="_blank" rel="noreferrer" className={styles.projectLink}>Link</a>
 
--- templates/components/resourceSlideAnim.tsx
+++ templates/components/resourceSlideAnim.tsx
@@ -3,10 +3,8 @@ import styles from "@/styles/resourcesSlide.module.scss";
 
 const ResourceSlideAnim: React.FC<{ title: string; description: string; link: string }> = (props: { title: string; description: string; link: string }): JSX.Element => {
     return (
-        <div className="resourceSlideAnim">
-            <p>{props.title}</p>
-            <p>{props.description}</p>
-            <a href={props.link} target="_blank" rel="noreferrer">Link</a>
+        <div className={styles.resourceCard}>
+            <img src="/images/scrollDown.svg" className={styles.scrollDown} />
         </div>
     );
 };
