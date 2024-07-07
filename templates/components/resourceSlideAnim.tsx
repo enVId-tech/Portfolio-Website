38d89d0154e5a0fe5dbd865f265974ff7e3fd56a
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-06 19:32:17 -0700

    25-style. Added styling to resources

--- templates/components/resourceSlideAnim.tsx
+++ templates/components/resourceSlideAnim.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Image from "next/image";
 import styles from "@/styles/resourcesSlide.module.scss";
 
 const ResourceSlideAnim: React.FC<{ title: string; description: string; link: string }> = (props: { title: string; description: string; link: string }): JSX.Element => {
     return (
         <div className={styles.resourceCard}>
-            <img src="/images/scrollDown.svg" className={styles.scrollDown} />
+            <Image src="/images/scrollDown.svg" alt="Scroll Down" width={50} height={50} className={styles.image}/>
         </div>
     );
 };
