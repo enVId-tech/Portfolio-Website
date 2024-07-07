5a3bc9cb1f00b4299940b835d304472f5e48605f
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-06 21:15:53 -0700

    27-style. Added more styling for resource slides

--- templates/components/resourceSlideAnim.tsx
+++ templates/components/resourceSlideAnim.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Image from "next/image";
 import styles from "@/styles/resourcesSlide.module.scss";
+import { Montserrat, Work_Sans } from "next/font/google";
 
 interface ResourceSlideAnimProps {
     title: string;
@@ -11,10 +11,24 @@ interface ResourceSlideAnimProps {
     link: string;
 }
 
+const Work_Sans400 = Work_Sans({
+    weight: "400",
+    style: 'normal',
+    subsets: ['latin']
+});
+
+const Montserrat400 = Montserrat({
+    weight: "400",
+    style: 'normal',
+    subsets: ['latin']
+});
+
 const ResourceSlideAnim: React.FC<ResourceSlideAnimProps> = (props: ResourceSlideAnimProps): JSX.Element => {
     return (
         <div className={styles.resourceCard}>
             <img src={props.image} width={50} height={50} className={styles.image} />
+            <h3 className={`${styles.title} ${Work_Sans400.className}`}>{props.title}</h3>
+            <p className={`${styles.description} ${Montserrat400.className}`}>{props.description}</p>
         </div>
     );
 };
