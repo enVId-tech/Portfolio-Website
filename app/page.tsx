eba2e45c1cf187ab59a77ecd6a35d8d419404bce
Erick Tran <thelittlebotengineer@gmail.com>
2025-03-11 18:18:32 -0700

    feat(project): remove old project, add dot background

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,18 @@
-"use client";
+"use server"
 import React from "react";
-import Head from "next/head";
 import styles from '@/styles/home.module.scss';
-import TitleDiv from "@/components/titleDiv.section";
-import About from "@/components/about.section";
-import Footer from "@/components/footer.section";
-import Projects from "@/components/projects.section";
-import Resources from "@/components/resources.section";
+import DotBackground from "@/app/_components/dotbackground.tsx";
 
-// JSON Data
-import { ResourcesProps } from "@/public/json/resources.section.ts";
-import { AboutProps } from "@/public/json/about.section.ts";
-import { TitleProps } from "@/public/json/title.section.ts";
-import { ProjectProps } from "@/public/json/project.section.ts";
-import { FooterProps } from "@/public/json/footer.section.ts";
-import TopNavBar from "@/components/topNavBar.section";
-import Contact from "@/components/contact.section";
-
-const HomePage: React.FC = (): React.JSX.Element => {
+export default async function HomePage(): Promise<React.ReactElement> {
     return (
-        <div className={styles.homePageMainDiv}>
-            <TopNavBar />
-            <TitleDiv
-                titlePlate={TitleProps.titlePlate as string}
-                subTitlePlate={TitleProps.subTitlePlate as string}
-                titlePlateDelay={TitleProps.titlePlateDelay as number}
-                subTitlePlateDelay={TitleProps.subTitlePlateDelay as number}
-                timeBetweentitleAndSubTitle={TitleProps.timeBetweentitleAndSubTitle as number}
-                waitTime={TitleProps.waitTime as number}
-            />
-            <About aboutScrollHeight={AboutProps.aboutScrollHeight as number} aboutText={AboutProps.aboutText as string[]} />
-            <Resources resources={ResourcesProps.resources as { title: string; description: string; image: string; link: string; }[]} />
-            <Projects projects={ProjectProps.projects as { title: string; description: string; embed: string; link: string; githubLink: string; techStack: string[]; }[]} />
-            <Contact />
-            <Footer
-                latestUpdate={FooterProps.latestUpdate as string}
-                dateUpdated={FooterProps.dateUpdated as string}
-                brandName={FooterProps.brandName as string}
-            />
-        </div>
-    );
-};
+        <DotBackground config={{
+            targetDotCount: 100,
+            dotSize: 0.75,
+            dotColor: 'rgb(200, 200, 255)',
+            dotOpacity: 0.8,
+            maxDistance: 120
+        }}>
 
-export default HomePage;
+        </DotBackground>
+    )
+}
